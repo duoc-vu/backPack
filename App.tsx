6585ec3070cef46faba0dc17a02528efc88cdad7
duocvu <bosschanel19@gmail.com>
2024-04-16 10:56:44 +0700

    Thay doi App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import React from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeTab from './Screens/HomeTab';
+import LoginTab from './Screens/LoginTab';
 
 
+const Tab = createBottomTabNavigator();
 const App = () => {
   return(
-    <NavigationContainer>
-      <></>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeTab} />
+      <Tab.Screen name="Settings" component={LoginTab} />
+    </Tab.Navigator>
   )
 }
 
