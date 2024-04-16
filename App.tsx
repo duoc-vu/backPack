c4565b9f2519d3943519e6a33e9d4cc832e832a3
duocvu <bosschanel19@gmail.com>
2024-04-16 12:03:27 +0700

    Vector Main

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeTab from './Screens/HomeTab';
+import LoginTab from './Screens/LoginTab';
 
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return(
     <NavigationContainer>
-      <></>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeTab} />
+        <Tab.Screen name="Login" component={LoginTab} />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
--- Screens/LoginTab.tsx
+++ Screens/LoginTab.tsx
@@ -1,9 +1,11 @@
 import React, { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const LoginTab = () => {
     return(
         <>
             <View>
+                <Icon name="rocket" size={30} color="#900" />
                 <Text>Login Tab</Text>
             </View>
         </>
