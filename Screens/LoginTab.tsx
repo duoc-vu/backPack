bbfb115c4642d38e81418445395012ca29e0a259
duocvu <bosschanel19@gmail.com>
2024-04-16 10:52:05 +0700

    Khoi tao

---
+++ Screens/LoginTab.tsx
@@ -0,0 +1,13 @@
+import React, { Text, View } from 'react-native';
+
+const LoginTab = () => {
+    return(
+        <>
+            <View>
+                <Text>Login Tab</Text>
+            </View>
+        </>
+    )
+}
+
+export default LoginTab;
