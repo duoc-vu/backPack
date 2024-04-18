48c0c69d2896ec171185c3f43c17bf2ceaa26293
duocvu <bosschanel19@gmail.com>
2024-04-18 11:13:10 +0700

    ThemLogin

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
-import React from 'react-native';
+
+import React, { Button } from 'react-native';
+import { Text } from 'react-native';
+import HomeTab from './Screens/HomeTab';
+import { View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginTab from './Screens/LoginTab';
 import { NavigationContainer } from '@react-navigation/native';
 
+const HomeMenu = ({navigation}:any) => {
+  return(
+      <View>
+        <Text>Home Menu</Text>
+        <Button title='Chuyen trang Login' onPress={()=> navigation.navigate('Login')}></Button>
+      </View>
+
+  )
+};
+
+const Detail = () => {
+  return(
+    <View>
+      <Text>Detail Screen</Text>
+    </View>
+  )
+}
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return(
+
     <NavigationContainer>
-      <></>
-    </NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeMenu" component={HomeMenu} />
+        <Stack.Screen name='Home' component={HomeTab}/>
+        <Stack.Screen name="Login" component={LoginTab}/>
+      </Stack.Navigator>
+      </NavigationContainer>
   )
 }
-
-export default App;
+export default App
