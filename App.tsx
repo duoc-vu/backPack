781e98eceb0a274bd098a73cf03b378f04c0dee1
duocvu <21A100100100@students.hou.edu.vn>
2024-06-01 00:09:19 +0700

    Configure and test

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
-import React from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import firebase, { query } from '@react-native-firebase/firestore';
+import { Text, View } from 'react-native';
 
 const App = () => {
-  return(
+  const [sizes, setSizes] = useState([]);
+
+  useEffect(() => {
+    const fb = firebase().collection('sizes');
+    fb.onSnapshot(querySnapshot => {
+      const ls: any = [];
+      querySnapshot.forEach((doc:any) => {
+        ls.push({
+          id: doc.data().id,
+          name: doc.data().name,
+          value: doc.data().value,
+        });
+      });
+      console.log(ls);
+      setSizes(ls);
+    });
+  }, []);
+
+  return (
     <NavigationContainer>
-      <></>
+      <View>
+        {sizes.map( item=>{
+          return (<Text>{item.name}</Text>)
+        })}
+      </View>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
